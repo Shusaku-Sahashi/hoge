9e17ac9531a3f886c618c1cd6b51504a3598d31e
monors <shu.sahashi.study@gmail.com>
2019-02-02 16:30:42 +0900

    単体テストのライブラリを追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const portfinder = require("portfinder");
+const backend = require("./build/dev-server");
+
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
+  },
+  devServer: {
+    before: backend,
+    proxy: {
+      // apiを呼び出す場合にInterseptして開発サーバーにリクエストを行う様に設定可能。
+      "/api": {
+        // '/api'から始まるリクエストを'api.yourservice.com'にプロキシする。
+        target: "http://api.yourservice.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
   }
 };
